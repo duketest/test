c099f66d66dbbd5aed150cf0bd25308796139347
Lubos <update@q7.sk>
2013-08-19 15:05:21 +0200

    start project

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+	<title>Test</title>
+</head>
+<style type="text/css"></style>
+<body>
+	<h1>Lorem ipsum dolor sit amet</h1>
+	<p>
+		Ut urna justo, placerat vel tempor nec, vestibulum in lacus. Sed mi orci, hendrerit quis hendrerit eget, pharetra eget nunc. Maecenas sodales tincidunt facilisis. Cras vulputate est sit amet pretium auctor. Donec in mi eu nibh tristique egestas. Nunc ut pretium diam. Nulla vestibulum semper accumsan. Vivamus sit amet est aliquet, malesuada eros ut, placerat mi. Praesent posuere lobortis justo vel dictum. Cras viverra faucibus nibh quis molestie.
+	</p>
+</body>
+</html>
